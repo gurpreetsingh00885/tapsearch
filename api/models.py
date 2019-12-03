f3625205d424188492d2011efce24cd9687c5b86
Gurpreet Singh <gurpreetsingh00885@gmail.com>
2019-12-03 06:40:02 +0530

    models.py: Add Paragraph and Word models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+MAX_TEXT_LENGTH = 1000000
+
+class Paragraph(models.Model):
+  text = models.CharField(max_length=MAX_TEXT_LENGTH)
+
+
+class Word(models.Model):
+	word = models.CharField(max_length=1000)
+	paragraphs = models.ManyToManyField(Paragraph)
+
